ab93e2b25e50fcb01a25dfdf2592831b3d8040ec
samantrs <https://samantrs@git.rabobank.nl>
2023-08-01 22:13:20 +0200

    remove unused imports

--- kafka-events-consumer/src/main/java/com/microservices/kafkaeventconsumer/repository/FailureRecordRepository.java
+++ kafka-events-consumer/src/main/java/com/microservices/kafkaeventconsumer/repository/FailureRecordRepository.java
@@ -4,8 +4,6 @@ import com.microservices.kafkaeventconsumer.entity.FailureRecordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface FailureRecordRepository extends JpaRepository<FailureRecordEntity, Integer> {
 
